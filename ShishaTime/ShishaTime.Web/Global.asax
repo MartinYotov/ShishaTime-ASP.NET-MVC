15f2ae24db7d242e1180e235f212cc6ba4c07930
MartinYotov <m.yotov96@gmail.com>
2017-03-16 02:36:25 +0200

    Project Layout

---
+++ ShishaTime/ShishaTime.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShishaTime.Web.MvcApplication" Language="C#" %>
---
+++ ShishaTime/ShishaTime.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShishaTime.Web.Startup))]
+namespace ShishaTime.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
