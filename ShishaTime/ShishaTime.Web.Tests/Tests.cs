27acd63b7dbc8de11528407732dc4b5b41364078
MartinYotov <m.yotov96@gmail.com>
2017-03-16 12:36:34 +0200

    Testing Jenkins

---
+++ ShishaTime/ShishaTime.Web.Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdsHub.Web.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsFalse(false);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int a = 5;
+
+            Assert.AreEqual(5, a);
+        }
+    }
+}
